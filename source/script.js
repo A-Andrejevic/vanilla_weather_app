100b55499959b1eae08061648e7f53a08cd0a482
A-Andrejevic <92856864+A-Andrejevic@users.noreply.github.com>
2021-10-22 13:07:42 +0200

    Added weather API and time in JS

---
+++ source/script.js
@@ -0,0 +1,44 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
+function displayCurrentWeather(response) {
+  let cityElement = document.querySelector("h1");
+  cityElement.innerHTML = response.data.name;
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let descriptionElement = document.querySelector("#weather-description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = response.data.wind.speed;
+  let timeElement = document.querySelector("#time");
+  timeElement.innerHTML = formatDate(response.data.dt * 1000);
+}
+
+let cityName = "New York";
+let apiKey = "745e466f4597986491c458e2888a3c22";
+let endPoint = "https://api.openweathermap.org/data/2.5/weather?";
+let apiUrl = `${endPoint}q=${cityName}&units=metric&appid=${apiKey}`;
+
+axios.get(apiUrl).then(displayCurrentWeather);
